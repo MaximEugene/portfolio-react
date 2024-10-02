0207575b1cdcc6bd4071f3e0064ff0386e30c5f2
Maxim E <MaximusEugene@hotmail.com>
2024-10-02 05:38:03 -0700

    Added router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Home from "./views/home";
+import Expertise from "./views/expertise/index.js";
 import "./App.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "expertise",
+    element: <Expertise />,
+  },
+]);
+
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/views/expertise/index.js
@@ -0,0 +1,15 @@
+import "./expertise.css";
+
+const Expertise = () => {
+  return (
+    <div className="expertise">
+      <div>asdfasd</div>
+      <div>asdfasd</div>
+      <div>asdfasd</div>
+      <div>asdfasd</div>
+      <div>asdfasd</div>
+    </div>
+  );
+};
+
+export default Expertise;
