eec93840e8d038f252902ac778293241f822c79d
Maxim E <MaximusEugene@hotmail.com>
2024-10-01 19:44:31 -0700

    Add welcome loader

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,6 +1,13 @@
 import "./loader.css";
 
 const Loader = () => {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="loader">
+      <div>
+        Welcome to <span className="loader-name">Maxim Eugene</span>'s Website!
+      </div>
+      <div className="loader-loading">...loading now</div>
+    </div>
+  );
 };
 export default Loader;
